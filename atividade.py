384cdb54479e8fef9e33c7153abf9ee6087f4f9b
Lucas Dantas <luk.dantas@gmail.com>
2018-08-17 01:19:19 -0300

    atividade python
    atividade percorrer diretorio usando os.walk

--- atividade.py
+++ atividade.py
@@ -1,19 +1,14 @@
 import os
-
-path = r"C:\Users\Pichau\Desktop\PythonFiles"
-
-#print(os.listdir(path))
-
+path = "."
 def pesquisar(conteudo):
-    for roots, dirs, files in os.walk(path): #duvida: sem roots dá error
+    for _, dirs, files in os.walk(path): 
         for file in files:
             if conteudo in file:
                 print("Arquivo - %s Tipo:" % file, type(file))
         for dir in dirs:
             if conteudo in dir:
                 print("Diretorio - %s Tipo:" % dir, type(dir))
-   # for root in roots:
-       # print("Raiz - %s" % roots)
-
+                
 pesquisar("foto")
+
 
